adcf9efd3611aeb009a5ced1651245e748071e9a
Nick Telenson <ntelenson@gmail.com>
2019-11-14 20:04:32 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,7 @@
-export function managePresents(state, action){
+export function managePresents(state = { numberOfPresents: 0}, action){
+
+  switch(action.type){
+    case "INCREASE":
+      return {...prevState, numberOfPresents: prevState.numberOfPresents + 1 }
+  }
 }
